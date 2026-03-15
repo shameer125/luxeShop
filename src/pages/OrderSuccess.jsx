4dc8b8f441e51c592a4533e0a5bd2d1e9de34096
shameer125 <alishameer251@gmail.com>
2026-03-15 16:47:27 -0700

    updates in ordersuccess part

--- src/pages/OrderSuccess.jsx
+++ src/pages/OrderSuccess.jsx
@@ -18,6 +18,7 @@ const OrderSuccess = () => {
           <CheckCircle2 size={48} />
         </motion.div>
 
+        
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
